fc02bc3ab6c8db6f8fe04124df8d681b4c42b094
tezjavi@gmail.com <javiermurillocota@MacBook-Pro-de-Javier.local>
2022-10-26 15:14:50 -0600

    Se cambio de proyecto

---
+++ Recervaciones-app/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/saludo', function () {
+    return "hola javier";
+});  
+
+Route::get('/saludo/{name}', function ($name) {
+    return "hola " .$name;
+});
+
+Route::get('/suma/{num1}/{num2}', function ($num1, $num2) { 
+    return $num1 + $num2;
+})->where(['num1' => '[0-9]+', 'num2'=> '[0-9]+']);
+
+Route::get('/multi/{num1}/{num2}/{num3?}', function ($num1, $num2,$num3=1) { 
+    return $num1 * $num2 * $num3;
+});
