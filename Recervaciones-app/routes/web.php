05163fe2798f89ef7f2a908e88fd8d403f4ea494
tezjavi@gmail.com <javiermurillocota@MacBook-Pro-de-Javier.local>
2022-10-27 19:14:34 -0600

    Se crean las rutas de clientes

--- Recervaciones-app/routes/web.php
+++ Recervaciones-app/routes/web.php
@@ -17,22 +17,17 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// User RouteS
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/create', [UserController::class, 'create']); 
 Route::get('/users/{id}', [UserController::class, 'show']);
 Route::post('/users', [UserController::class, 'store']);
-// Route::get('/saludo', function () {
-//     return "hola javier";
-// });  
 
-// Route::get('/saludo/{name}', function ($name) {
-//     return "hola " .$name;
-// });
 
-// Route::get('/suma/{num1}/{num2}', function ($num1, $num2) { 
-//     return $num1 + $num2;
-// })->where(['num1' => '[0-9]+', 'num2'=> '[0-9]+']);
+// Clients Routes
 
-// Route::get('/multi/{num1}/{num2}/{num3?}', function ($num1, $num2,$num3=1) { 
-//     return $num1 * $num2 * $num3;
-// });
+Route::get('/clients', [ClientController::class, 'index']);
+Route::get('/clients/create', [ClientController::class, 'create']); 
+Route::get('/clients/{id}', [ClientController::class, 'show']);
+Route::post('/clients', [ClientController::class, 'store']);
